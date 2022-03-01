43ef752b1fe3b6743ed0a89b00967d5fab8cd1a7
abcdabcdabcda <98587050+abcdabcdabcda@users.noreply.github.com>
2022-03-01 17:26:13 -0500

    Update gaussian.py

--- likelihoods/gaussian.py
+++ likelihoods/gaussian.py
@@ -14,8 +14,7 @@ class Gaussian(likelihood.Likelihood):
         return None
 
     def predict(self, latent_val):
-        # std = tf.exp(self.log_var / 2.0)
-        return latent_val  # + std * tf.random_normal([1, tf.shape(latent_val)[1], 1])
+        return latent_val
 
     def get_name(self):
         return "Regression"
