df5940805859a27654f8ff27beb02a1d41fa2e15
abcdabcdabcda <98587050+abcdabcdabcda@users.noreply.github.com>
2022-03-01 17:26:26 -0500

    Update likelihood.py

--- likelihoods/likelihood.py
+++ likelihoods/likelihood.py
@@ -7,8 +7,8 @@ class Likelihood:
     def log_cond_prob(self, output, latent_val):
         """
         Subclass should implement log p(Y | F)
-        :param output:  (batch_size x Dout) matrix containing true outputs
-        :param latent_val: (MC x batch_size x Q) matrix of latent function values, usually Q=F
+        :param output:  (size x Dout) matrix containing true outputs
+        :param latent_val: (MC x size x Q) matrix of latent function values, usually Q=F
         :return:
         """
         raise NotImplementedError("Subclass should implement this.")
