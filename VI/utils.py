b19e2b5d6ff7ca7eb62ee9130ce51203059fabf8
abcdabcdabcda <98587050+abcdabcdabcda@users.noreply.github.com>
2022-03-02 12:43:45 -0500

    Add files via upload

--- VI/utils.py
+++ VI/utils.py
@@ -55,12 +55,12 @@ def get_flags():
     flags.DEFINE_string('optimizer', "adagrad", 'Optimizer')
     flags.DEFINE_string('kernel_type', "RBF", 'arccosine')
     flags.DEFINE_integer('kernel_arccosine_degree', 1, 'Degree parameter of arc-cosine kernel')
-    flags.DEFINE_boolean('is_ard', False, 'Using ARD kernel or isotropic')
-    flags.DEFINE_boolean('local_reparam', False, 'Using the local reparameterization trick')
+    flags.DEFINE_boolean('is_ard', True, 'Using ARD kernel or isotropic')
+    flags.DEFINE_boolean('local_reparam', True, 'Using the local reparameterization trick')
     flags.DEFINE_boolean('feed_forward', False, 'Feed original inputs to each layer')
     flags.DEFINE_integer('q_Omega_fixed', 0, 'Number of iterations to keep posterior over Omega fixed')
     flags.DEFINE_integer('theta_fixed', 0, 'Number of iterations to keep theta fixed')
-    flags.DEFINE_string('learn_Omega', 'prior_fixed', 'How to treat Omega - fixed (from the prior), optimized, or learned variationally')
+    flags.DEFINE_string('learn_Omega', 'var_fixed', 'How to treat Omega - fixed (from the prior), optimized, or learned variationally')
     flags.DEFINE_integer('duration', 10000000, 'Duration of job in minutes')
 
     # Flags for use in cluster experiments
